3cae5041968b8f7be6ae6d258ba32031bb44c535
floquino1fef <julianovasconcelos39@gmail.com>
2024-09-07 12:10:16 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,49 @@
+let dados =  [
+    
+  {
+    titulo : "os caça fantasmas",
+    descricao : "Os Caça-Fantasmas é um clássico do cinema que misturou comédia e terror de forma hilária e memorável. A trama acompanha três cientistas que, após serem demitidos da universidade, decidem transformar sua paixão pelo paranormal em um negócio: a exterminadora de fantasmas 'Ghostbusters'.",
+    link : "https://pt.wikipedia.org/wiki/Os_Ca%C3%A7a-Fantasmas",
+    tags : "filme"
+  },
+
+  {
+    titulo : " egon spengler",
+
+      descricao : " interpretado por Harold Ramis, é um dos fundadores do grupo Caça Fantasmas. Conhecido por sua inteligência e paixão pela ciência, Egon é a mente por trás de muitos dos equipamentos utilizados pela equipe. Sua frase icônica quem voce vai chamar? se tornou um símbolo da franquia.",
+      link : "https://pt.wikipedia.org/wiki/Os_Ca%C3%A7a-Fantasmas",
+      tags : "fundador"
+  },
+  {
+    titulo : "peter Vekman",
+
+    descricao : "interpretado por Bill Murray, Peter Venkman é o líder carismático e um tanto quanto cínico dos Caça-Fantasmas. Conhecido por seu charme e senso de humor, ele é responsável por atrair a maioria dos clientes para a empresa. Sua frase icônica 'Eu acredito em você' se tornou um dos momentos mais memoráveis da franquia.",
+    link : "https://pt.wikipedia.org/wiki/Os_Ca%C3%A7a-Fantasmas",
+    tags : "Bill"
+  },
+    {
+      titulo :  "winston Zeddemore",
+    descricao : "interpretado por Ernie Hudson, Winston Zeddemore é o mais novo membro da equipe dos Caça-Fantasmas. Ex-fuzileiro naval, ele traz uma perspectiva mais prática e realista para o grupo. Sua lealdade e bom humor são essenciais para manter o moral da equipe alta.",
+    link : "https://pt.wikipedia.org/wiki/Os_Ca%C3%A7a-Fantasmas",
+    tags : "ernie"
+    },
+   {
+      titulo :  "ray Stantz",
+
+      descricao : "interpretado por Dan Aykroyd, Ray Stantz é o mais entusiasmado e infantil dos Caça-Fantasmas. Ele é um grande fã do paranormal e acredita firmemente na existência de fantasmas. Sua habilidade com o PKE meter é fundamental para localizar entidades espirituais.",
+      link : "https://pt.wikipedia.org/wiki/Os_Ca%C3%A7a-Fantasmas",
+      tags : "etusiasmado"
+    },
+    {
+      titulo :  "gozer",
+      descricao : "Gozer, a Destruidora, é uma divinidade antiga e poderosa que ameaça destruir Nova York. Ela é invocada acidentalmente pelos Caça-Fantasmas e se torna a principal antagonista do primeiro filme.",
+      link : "https://pt.wikipedia.org/wiki/Os_Ca%C3%A7a-Fantasmas",
+      tags : "fantasma asombração asombraçao "
+      
+    }
+
+]
+
+
+
+    
